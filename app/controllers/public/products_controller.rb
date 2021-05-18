6e54336bc82ead0713487c77a36b701287da2e7a
EC2 Default User <ec2-user@ip-172-31-2-154.ec2.internal>
2021-05-18 07:18:55 +0000

    product-show,index-first

--- app/controllers/public/products_controller.rb
+++ app/controllers/public/products_controller.rb
@@ -1,7 +1,9 @@
 class Public::ProductsController < ApplicationController
   def index
+    @products = Product.page(params[:page]).reverse_order
   end
 
   def show
+    @product = Product.find(params[:id])
   end
 end
