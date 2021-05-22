dbdf7de950566c21caab91568d8f79e2817e8781
EC2 Default User <ec2-user@ip-172-31-31-83.ec2.internal>
2021-05-22 13:35:41 +0000

    [Add]admin/product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,14 +1,20 @@
 class Product < ApplicationRecord
-    
+
   belongs_to :genre
   has_many :order_items, dependent: :destroy
   has_many :cart_items, dependent: :destroy
-  
+
   attachment :image
-  
+
   validates :name, presence: true
   validates :description, presence: true
   # numericality〜整数のみというバリデーションらしいので記述
   validates :price, presence: true, numericality: { only_integer: true }
-  
+	
+
+# 税込価格の表記
+  def add_tax_price
+   (self.price * 1.10).round
+  end
+
 end
