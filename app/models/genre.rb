84739aa534e42b985a8dc5f67068a2ab4f1b63a0
EC2 Default User <ec2-user@ip-172-31-2-154.ec2.internal>
2021-05-25 07:13:45 +0000

    fix-derivery-controller

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -2,15 +2,12 @@ class Genre < ApplicationRecord
 
   has_many :products, dependent: :destroy
   
-<<<<<<< HEAD
   def self.search(search, word)
     @genres = Genre.where("name LIKE?","%#{word}%")
   end
 
-=======
   def self.searchgenre(word)
     where(["name LIKE?", "%#{word}%"])
   end
   
->>>>>>> origin/develop
 end
