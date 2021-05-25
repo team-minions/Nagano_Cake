0a7d7dd7df3f5d789851a45b7dc2538d00cce97f
EC2 Default User <ec2-user@ip-172-31-31-83.ec2.internal>
2021-05-25 01:41:04 +0000

    ステータス修正

--- app/controllers/admin/order_items_controller.rb
+++ app/controllers/admin/order_items_controller.rb
@@ -8,10 +8,10 @@ class Admin::OrderItemsController < ApplicationController
 
     if @order_item.making_status == "製作中"
       @order.update(status: 2)
-    elsif @order.order_items.count == @order.order_items.where(making_status: "制作完了").count
+    elsif @order.order_items.count == @order.order_items.where(making_status: "製作完了").count
       @order.update(status: 3)
     end
-    redirect_to admin_order_path(@order), flash: {success: "制作ステータス変更"}
+    redirect_to admin_order_path(@order), flash: {success: "製作ステータス変更"}
   end
 
   private
